5523c8ac3711baec6cf7dc8e0246dbe7c11459da
amalshehu <amalshehu@gmail.com>
2018-04-10 10:33:41 +0530

    Add collision detector

--- main.js
+++ main.js
@@ -57,6 +57,26 @@ Ball.prototype.update = function() {
   this.x += this.velX
   this.y += this.velY
 }
+Ball.prototype.collisionDetect = function() {
+  for (var j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      var dx = this.x - balls[j].x
+      var dy = this.y - balls[j].y
+      var distance = Math.sqrt(dx * dx + dy * dy)
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color =
+          'rgb(' +
+          random(0, 255) +
+          ',' +
+          random(0, 255) +
+          ',' +
+          random(0, 255) +
+          ')'
+      }
+    }
+  }
+}
 
 var balls = []
 
@@ -85,8 +105,8 @@ function loop() {
   for (var i = 0; i < balls.length; i++) {
     balls[i].draw()
     balls[i].update()
+    balls[i].collisionDetect()
   }
-
   requestAnimationFrame(loop)
 }
 loop()
